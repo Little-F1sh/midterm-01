cd5a81e471368babb6e0049efd9468c93c99243a
Little-F1sh <itsnihon1@gmail.com>
2023-07-30 19:29:52 +0600

    shop

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*================ Navbar section ===================*/
 const searchForm = document.querySelector('.search-form');
 const searchBtn = document.querySelector('#search-btn');
 
@@ -18,3 +19,43 @@ const cartBtn = document.querySelector('#cart-btn');
 cartBtn.onclick = function() {
     cartBox.classList.toggle('active')
 }
+
+/*================ Slider ===================*/ 
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0:{
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },        
+        640:{
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        824:{
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1024:{
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+        1324:{
+            slidesPerView: 5,
+            spaceBetween: 30,
+        },
+    },
+  });
