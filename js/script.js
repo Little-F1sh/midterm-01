ed6b557fa0181d14333318270c2010a1166f8a44
Little-F1sh <itsnihon1@gmail.com>
2023-08-01 16:50:49 +0600

    have to work more

--- js/script.js
+++ js/script.js
@@ -1,24 +1,5 @@
 /*================ Navbar section ===================*/
-const searchForm = document.querySelector('.search-form');
-const searchBtn = document.querySelector('#search-btn');
 
-searchBtn.onclick = function() {
-    searchForm.classList.toggle('active')
-}
-
-const loginForm = document.querySelector('.login-form');
-const loginBtn = document.querySelector('#login-btn');
-
-loginBtn.onclick = function() {
-    loginForm.classList.toggle('active')
-}
-
-const cartBox = document.querySelector('.cart-box');
-const cartBtn = document.querySelector('#cart-btn');
-
-cartBtn.onclick = function() {
-    cartBox.classList.toggle('active')
-}
 
 /*================ Slider ===================*/ 
 
