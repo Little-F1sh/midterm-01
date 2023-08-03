5f977ed60fa92ea900abab2706c3fdbf1895106e
Little-F1sh <itsnihon1@gmail.com>
2023-08-03 06:16:50 +0600

    searchvbardone

--- js/script.js
+++ js/script.js
@@ -40,3 +40,83 @@ var swiper = new Swiper(".mySwiper", {
         },
     },
   });
+
+  const products = [
+    {
+        id: 0,
+        image: 'images/c_formal_gray_shirt.png',
+        title: 'shirt',
+        price: 29.99
+    },
+    {
+        id: 1,
+        image: 'images/c_pant_girl.png',
+        title: 'pant',
+        price: 29.99
+    },
+    {
+        id: 2,
+        image: 'images/c_polo-shirt.png',
+        title: 'shirt',
+        price: 29.99
+    },
+    {
+        id: 3,
+        image: 'images/c_shirt-girl.png',
+        title: 'shirt',
+        price: 29.99
+    },
+];
+
+// Get unique product titles to use for search filtering
+const categories = [...new Set(products.map(item => item.title))];
+
+document.getElementById('searchbar').addEventListener('keyup', (e) => {
+    const searchData = e.target.value.toLowerCase();
+    const filterData = products.filter((item) => {
+        return item.title.toLowerCase().includes(searchData);
+    });
+    displayItem(filterData);
+});
+
+const displayItem = (items) => {
+    document.getElementById('products').innerHTML = items.map((item) => {
+        var { id, image, title, price } = item;
+        return (
+            `<div class='box'>
+                <img class='images' src=${image}></img>
+                <h5>${title}</h5>
+                <span>${price}.00</span>
+                <button onclick="addToCart(${id})">Add to Cart</button>
+            </div>`
+        );
+    }).join('');
+};
+
+// Initially display all products
+displayItem(products);
+
+const cartItems = [];
+
+// Function to add items to the cart
+const addToCart = (productId) => {
+    const selectedProduct = products.find(item => item.id === productId);
+    if (selectedProduct) {
+        cartItems.push(selectedProduct);
+        displayCart();
+    }
+};
+
+// Function to display cart items
+const displayCart = () => {
+    document.getElementById('cart').innerHTML = cartItems.map((item) => {
+        var { id, image, title, price } = item;
+        return (
+            `<div class='box'>
+                <img class='images' src=${image}></img>
+                <h5>${title}</h5>
+                <span>${price}.00</span>
+            </div>`
+        );
+    }).join('');
+};
